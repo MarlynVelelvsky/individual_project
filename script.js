0d947f09bd5c4402ff1c526a90659f71e849d6e1
MarlynVelelvsky <marin.ka.15162@gmail.com>
2024-12-06 21:04:30 +0100

    commit 15 comments

--- script.js
+++ script.js
@@ -7,5 +7,17 @@ document.getElementById("add-comment").addEventListener("click", function() {
         document.getElementById("comment-box").value = "";
     }
 }
-
 )
+
+const toggleButton = document.getElementById("toggle-comments");
+const commentSection = document.querySelector(".comment");
+
+toggleButton.addEventListener("click", function(){
+    if (commentSection.classList.contains("visible")) {
+        commentSection.classList.remove("visible");
+        toggleButton.textContent = "Comment";
+    } else {
+        commentSection.classList.add("visible");
+        toggleButton.textContent = "Hide Comments";
+    }
+});
